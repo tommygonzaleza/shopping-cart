e035260563c189ef33c0efc19da478436e2072c4
tommygonzaleza <tommygonzaleza@users.noreply.github.com>
2020-09-15 12:51:53 -0400

    Search input functioning and sort by added (Doesn't function).

--- front-end/src/js/views/ProductsView.js
+++ front-end/src/js/views/ProductsView.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom'
 import MyNavbar from '../components/MyNavbar';
 import ProductsData from '../ProductsData';
 
 const ProductsView = () => {
+    
+    const [search, setSearch] = useState("");
+
+    const handleChange = e => {
+        setSearch(e.target.value);
+    };
+
+    const results = !search ? ProductsData : ProductsData.filter((product) => 
+        product.name.includes(search)
+    );
+
     return (
         <div>
             <MyNavbar />
             <div className="section">
                 <h2>Products</h2>
+                <div>
+                    <input placeholder="Search" value={search} onChange={handleChange}></input>
+                    <form action="">
+                        <select id="sortby" name="sortby">
+                            <option value="" disabled selected>Sort By</option>
+                            <option value="Lower">Lower to Higher Price</option>
+                            <option value="Higher">Higher to Lower Price</option>
+                        </select>
+                    </form>
+                </div>
                 <div className="products">
-                    {ProductsData.map((product, idx) => (
+                    {results.map((product, idx) => (
                         <div className="product-card" key={idx}>
                             <Link to={`/product-details/${product.id}`}>
                                 <img src={product.image} alt={product.name} />
@@ -26,7 +47,6 @@ const ProductsView = () => {
                     ))}
                 </div>
             </div>
-            
         </div>
     );
 }
